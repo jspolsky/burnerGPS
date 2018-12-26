71d92cb1fe10a35af3d610be1ecf04c4d224a5a1
spolsky <spolsky@Jareds-iMac-3.home>
2018-12-26 12:03:18 -0500

    Initial experimenting with 24-pixel LED ring to get the visualization

---
+++ burnerGPS.ino
@@ -0,0 +1,82 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 9
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+
+// Gamma correction from https://learn.adafruit.com/led-tricks-gamma-correction/the-quick-fix
+
+const uint8_t PROGMEM gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
+
+uint32_t CorrectedWhite( byte w )
+{
+
+  uint8_t b = pgm_read_byte(&gamma8[w]);
+
+  return strip.Color(b,b,b);
+
+}
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+  strip.begin();
+  strip.setBrightness(64);
+  strip.show(); // Initialize all pixels to 'off'
+
+  Serial.begin(250000);
+}
+
+void loop() {
+
+
+
+  uint16_t i = 1023 - analogRead(10);
+
+  double pos = (double) i / 1024.0 * 24.0;
+  double dis = 255 * (pos - (double) (floor(pos))) ;
+  
+  strip.clear();
+  for (int i = 4; i <= 20; i+=1) strip.setPixelColor( i, strip.Color(0,24,0));
+  strip.setPixelColor( 0, strip.Color(0,0,64));
+  strip.setPixelColor( 6, strip.Color(0,0,64));
+  strip.setPixelColor( 12, strip.Color(0,0,64));
+  strip.setPixelColor( 18, strip.Color(0,0,64));
+  strip.setPixelColor( floor(pos), CorrectedWhite(255) );
+  strip.show();
+
+}
